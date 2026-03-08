7db779f5cfe204f750c129a51d5c64eead826c85
txt <txt1919810@gmail.com>
2026-03-08 17:24:25 +0800

    启动修正

--- src/rc_bringup/launch/sick+logical.launch.py
+++ src/rc_bringup/launch/sick+logical.launch.py
@@ -3,11 +3,11 @@ import os
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, ExecuteProcess
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
 from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
+
     # 获取 package 路径
     my_driver_dir = get_package_share_directory('my_driver')
     rc_bringup_dir = get_package_share_directory('rc_bringup')
@@ -33,6 +33,13 @@ def generate_launch_description():
     # MainLogic
     main_logic_process = ExecuteProcess(
         cmd=['python3', main_logic_path],
-        output='screen',
-        shell=True
+        output='screen'
     )
+
+    ld = LaunchDescription()
+
+    ld.add_action(mid360_launch)
+    ld.add_action(sick_slam_launch)
+    ld.add_action(main_logic_process)
+
+    return ld
