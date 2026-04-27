1353df1bef649388573ef31537f40acef6365c66
hjw132 <njustyyds@outlook.com>
2026-04-27 11:02:56 +0000

    serial

--- src/MainLogic/MAIN/R2_build_spear.py
+++ src/MainLogic/MAIN/R2_build_spear.py
@@ -14,7 +14,7 @@ from MainLogic.core.serial_node import start_serial_process
 
 
 async def async_main():
-    serial_port = "/dev/serial_qh"
+    serial_port = "/dev/serial_ch340"
     baudrate = 921600
     start_serial_process(serial_port=serial_port, baudrate=baudrate)
 
@@ -23,8 +23,8 @@ async def async_main():
     )
 
     bridge = ros_bridge_module.RosBridgeNodeInstance
-    bridge.register_serial_sub(gcb.mcu_transmit_callback)
-    bridge.register_serial_sub(gcb.serial_action_return_callback)
+    bridge.register_serial_sub(gcb.mcu_transmit_callback, 0xAA)
+    bridge.register_serial_sub(gcb.serial_action_return_callback, 0xA3)
     bridge.register_ros2_sub("qr_detection_result", gcb.ros_qr_callback, type=String)
     bridge.register_ros2_sub("spear_status", gcb.spear_callback, type=UInt8MultiArray)
     bridge.register_ros2_sub("/arucopnp/offset_mm", gcb.spear_offset_callback, type=PointStamped)
