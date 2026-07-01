95f10c0d686e83c123d2806f1a06aba02886c0b6
Elaina <1463967532@qq.com>
2026-07-01 07:25:52 +0000

    修改测试脚本

--- src/MainLogic/test/sick_correct.py
+++ src/MainLogic/test/sick_correct.py
@@ -3,35 +3,34 @@ import math
 from scipy.optimize import fsolve
 import numpy as np
 def main():
-    dyaw=5/180*math.pi
+    init_yaw=0.0*math.pi
+    dyaw=5/180*math.pi #旋转误差
     baseyaw=-5/180*math.pi #slam座标系下面的旋转
     init_x,init_y=0.39,4.6
     #slam真实座标系
-    map2init=Odom(init_x, init_y,dyaw)
-    #在倾斜座标系下面的差别
-    init2base=Odom(6,1.02,0)
-    map2base=map2init@init2base
-    #map座标系下观测的y
-    y_correct=map2base.y/math.cos(map2base.yaw+baseyaw)
-    y_left=(6.0-map2base.y)/math.cos(map2base.yaw)
-    #slam设置座标系
-    map2initset=Odom(init_x,init_y,0)
-    map2baseset=map2initset@init2base
-    #进行纠正
-    # dyaw=math.atan2(map2baseset.y-y_correct,map2baseset.x)
-    
-    # print(dyaw*180/math.pi)
-    dy=map2base.y-map2baseset.y
-    dx=map2base.x-map2baseset.x
+    map2init_real=Odom(init_x, init_y,dyaw+init_yaw) #带有初始值误差的真实起点
+    map2init_set=Odom(init_x,init_y,init_yaw) #代码设置的起点
+    # map2baseset=map2initset@init2base
+    #在地图座标系下面真实值
+    map2base_real=Odom(6.2,5.39,0.1*math.pi) #真实的移动
 
+    init2base_obs=map2init_real.inverse()@map2base_real #用真实的起点和真实的移动计算出来的观测值
+    init2base_real=map2init_set.inverse()@map2base_real #用代码设置的起点和真实的移动计算出来的slam观测值
+    y_real=map2base_real.y
+    y_cal=(map2init_set@init2base_obs)
+    print(f"odom真实值{init2base_real}\nslam观测值{init2base_obs}")
+    dx=init2base_real.x-init2base_obs.x
+    dy=init2base_real.y-init2base_obs.y
     print(f"dx:{dx},dy:{dy}")
-    #左侧的yaw
-    # dleft=min([fsolve(lambda theta: np.tan(theta) - (map2baseset.y - (6 / np.cos(theta) - y_left)) / map2baseset.x, guess)[0] for guess in (-0.5, 0.5)], key=abs) 
-    # print(dleft*180/math.pi)
-    # print(map2base)
-    # yaw=fsolve(lambda theta:-y_correct+map2base.x*math.sin(theta)+map2base.y*math.cos(theta)+init_y,0)[0]
-    yaw=fsolve(lambda theta:(-y_correct)*math.cos(theta+baseyaw)+init2base.x*math.sin(theta)+init2base.y*math.cos(theta)+init_y,0)[0]
-    # yaw=fsolve(lambda theta:-y_correct+Odom(init_x,init_y,theta)@init2base,0)[0]
+    
+    #slam设置座标系
+    yaw=fsolve(lambda theta:-y_real+init2base_obs.x*math.sin(theta+init_yaw)+init2base_obs.y*math.cos(theta+init_yaw)+init_y,0)[0]
+    #进行纠正
     print(yaw*180/math.pi)
+    
+    map2init_correct=Odom(init_x,init_y,init_yaw+yaw) #纠正后的起点
+    init2base_correct=map2init_correct.inverse()@map2base_real #纠正后的移动
+    map2base_correct=map2init_correct@init2base_correct #纠正后的slam移动
+    print(f"纠正后坐标{map2base_correct}")
 if __name__ == "__main__":
     main()
