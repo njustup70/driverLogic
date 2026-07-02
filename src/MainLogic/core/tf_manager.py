626ed689c1b2cb0e9df475f10b53aab49855df5c
Elaina <1463967532@qq.com>
2026-07-02 08:21:09 +0000

    纠正改用全车体坐标

--- src/MainLogic/core/tf_manager.py
+++ src/MainLogic/core/tf_manager.py
@@ -206,8 +206,9 @@ class TFManager:
         if not self.left_sick_buffer or not self.right_sick_buffer or not self._has_slam_pose:
             return False
         sick_y = sum(self.sick_buffer) / len(self.sick_buffer)
-        #先干掉纠正
-        self._mapToSlamInit = Odom(self._mapToSlamInit.x,self._mapToSlamInit.y,self._baseinitYaw)
+        self.mapToBaseInit=Odom(self.mapToBaseInit.x,self.mapToBaseInit.y,self._baseinitYaw)
+        #在当前座标系下求BaseInit->Base的值
+        baseinit2base=self.mapToBaseInit@self.laser_to_base.inverse()@self._slamBaseOdom
         # 先撤销上一轮修正，再基于未修正状态计算本轮修正量。
         #base_without_prev = Odom(0,0,-self._sickYawCorrection) @ self.baseLinkOdom.value
         #sick_pose = base_without_prev @ self.sickToBaseLink.inverse()
@@ -222,17 +223,17 @@ class TFManager:
                 #    - sick_y * math.cos(theta + self._baseinitodom.yaw) 
                 #    - self.sick_dist_to_base * math.sin(self.sick_yaw_in_base + theta + self._baseinitodom.yaw)
                 #)
-            dyaw =  fsolve(lambda theta:-calculate_y_real(theta)+self._baseinitodom.x*math.sin(theta+self._baseinitYaw)+self._baseinitodom.y*math.cos(theta+self._baseinitYaw)+self.mapToBaseInit.y,0)[0]
+            dyaw =  fsolve(lambda theta:-calculate_y_real(theta)+baseinit2base.x*math.sin(theta+self._baseinitYaw)+baseinit2base.y*math.cos(theta+self._baseinitYaw)+self.mapToBaseInit.y,0)[0]
 
         if self.sick_flag==1:
             dyaw = - fsolve(lambda theta:-sick_y*math.cos(theta+self._baseinitodom.yaw)+self._baseinitodom.x*math.sin(theta+self._baseinitYaw)+self._baseinitodom.y*math.cos(theta+self._baseinitYaw)+self.mapToBaseInit.y,0)[0]
         
         print(self._mapToSlamInit)
         #self._mapToSlamInit = Odom(0,0,-self._sickYawCorrection) @ Odom(0,0,nominal_yaw+new_yaw_correction) @ self._mapToSlamInit 
-        self._mapToSlamInit = Odom(self._mapToSlamInit.x,self._mapToSlamInit.y,self._baseinitYaw+dyaw)
+        self.mapToBaseInit=Odom(self.mapToBaseInit.x,self.mapToBaseInit.y,self._baseinitYaw+dyaw)
+        self._mapToSlamInit=self.mapToBaseInit@self.laser_to_base.inverse()
         print(self._mapToSlamInit)
 
-        self._sickYawCorrection = dyaw
 
         if self.rosBridge is not None:
             self.rosBridge.publish_static_tf(self.map_frame, self.slam_init_frame, self._mapToSlamInit)
