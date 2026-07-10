841e8ed97fb6185bd0ec34ec30768f2f936f8379
Chen Cheng <1940275781@qq.com>
2026-07-10 16:02:59 +0800

    fix.删除发布

--- src/MainLogic/core/tf_manager.py
+++ src/MainLogic/core/tf_manager.py
@@ -15,7 +15,6 @@ from MainLogic.Lib.bytes import turn_to_bytes
 from MainLogic.Lib.AsyncTools import AsyncVariable
 
 from MainLogic.core import ros_bridge_node as ros_bridge_module
-from MainLogic.Lib.Visual import PathVisualInstance
 BASE_LINK_ODOM_TOPIC = '/state/base_link_odom'
 
 class TFManager:
@@ -282,7 +281,6 @@ class TFManager:
         self._slamInitToOdom = slam_base_pose @ wheel_pose.inverse()
         self.rosBridge.publish_static_tf(self.slam_init_frame, self.odom_frame, self._slamInitToOdom)
         self.rosBridge.publish_static_tf(self.map_frame, self.slam_init_frame, self._mapToSlamInit)
-        PathVisualInstance.add_point("/state/base_link_path",self.baseLinkOdom.value)
     async def tf_update_loop(self):
         """统一更新任务：10ms 执行 odom 更新，每 100ms 执行一次 slam 更新。"""
         tick_10ms = 0
